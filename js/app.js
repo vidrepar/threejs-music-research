d69d6705d7654c3014c117af0ffa0d599409f4e9
vidrepar <vid.repar@gmail.com>
2016-07-10 12:05:48 +0200

    audio working: connected all the dots, visuals are responding to audio

--- js/app.js
+++ js/app.js
@@ -5,10 +5,24 @@ var app = {
     * core components
     * of any 3D environment
     * */
-
     scene   : null,
     camera  : null,
     renderer: null,
+
+    // Audio
+    sound:null,
+    analyser:null,
+    bufferLength:null,
+    dataArray:null,
+
+    // visual
+    sphere  : null,
+    sphereGeometry : null,
+
+    // audio
+    /*song    : null,
+    button    : null,
+    ftt    : null,*/
 
     PARTICLE_NUM:100,
 
@@ -44,35 +58,40 @@ var app = {
         container.append(app.renderer.domElement);
         $('body').append(container);
 
+
     },
-    drawParticles:function () {
+    setupAudio:function () {
 
-        var PI2 = Math.PI*2;
-
-        var material = new THREE.SpriteCanvasMaterial({
-            color: 0x000000,
-            program: function (context) {
-                context.beginPath();
-                context.arc(0,0,1,0,PI2,true);
-                context.fill();
-
-            }
+        // Audio setup
+        app.sound = new Pizzicato.Sound({
+            source: 'file',
+            options: { path: 'assets/sound/song-4.mp3' }
+        }, function() {
+            console.log('sound file loaded!');
+            app.sound.play();
         });
 
-        for (var i = 0; i < app.PARTICLE_NUM; i++) {
+        app.analyser = app.sound.getAnalyser();
 
-            /*
-             * Sprite() is just a holder
-             * */
-            var particle = new THREE.Sprite(material);
-            particle.position.x = Math.random() * 2 - 1;
-            particle.position.y = Math.random() * 2 - 1;
-            particle.position.z = Math.random() * 2 - 1;
-            particle.position.normalize();
-            particle.position.multiplyScalar(Math.random()*10+450);
-            app.scene.add(particle);
+        console.log(app.analyser);
 
-        }
+        app.analyser.fftSize = 256;
+        app.bufferLength = app.analyser.frequencyBinCount;
+        console.log(app.bufferLength);
+        app.dataArray = new Uint8Array(app.bufferLength);
+
+    },
+    drawSphere:function () {
+
+        app.sphereGeometry = new THREE.SphereGeometry( 25, 10, 10 );
+        var m = new THREE.MeshLambertMaterial( {color: 0x7777ff} );
+        app.sphere = new THREE.Mesh( app.sphereGeometry, m );
+
+        var light = new THREE.PointLight( 0xff0000, 1, 100 );
+        light.position.set( 50, 50, 50 );
+
+        app.scene.add( app.sphere );
+        app.scene.add( light );
 
     },
     render:function () {
@@ -87,6 +106,47 @@ var app = {
         /*
         * Animate stuff
         * */
+
+        // Audio based animation
+        app.analyser.getByteFrequencyData(app.dataArray);
+        //console.log(app.dataArray);
+
+        elmt = app.dataArray;
+
+        var sum = 0;
+        for( var i = 0; i < elmt.length; i++ ){
+            sum += parseInt( elmt[i], 10 ); //don't forget to add the base
+        }
+
+        var fttAvg = Math.floor(sum/elmt.length);
+
+        if (fttAvg > 0 && fttAvg < 65) {
+
+            //console.log('111');
+            app.sphere.rotation.y += 10;
+
+
+        } else if (fttAvg > 65 && fttAvg < 110) {
+
+            //console.log('222');
+            app.sphere.rotation.x += 7;
+
+        } else if (fttAvg > 110) {
+
+            //console.log('333');
+            app.sphere.rotation.y += 2;
+
+        }
+
+        console.log(fttAvg);
+
+        // Visual animation
+
+
+
+
+
+
         app.render();
 
     }
@@ -94,5 +154,6 @@ var app = {
 };
 
 app.init();
-app.drawParticles();
+app.drawSphere();
+app.setupAudio();
 app.animate();
